c565bd3dda6cf6d3c8d87d65436083e03af46027
steve-the-hermit <stevemacharia309@gmail.com>
2023-06-09 14:06:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Susan";
+const height = <40;
+const message = `${name}is less than${height} inches tall`;
 
 module.exports = {name, height, message};
